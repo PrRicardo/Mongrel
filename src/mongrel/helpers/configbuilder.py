c871c0e005b6f17daf1c4ff7dc3143df01936724
Ricardo <mail.prida@gmail.com>
2024-02-26 14:23:58 +0100

    Added configuration builder

--- src/mongrel/helpers/configbuilder.py
+++ src/mongrel/helpers/configbuilder.py
@@ -6,10 +6,10 @@ from map_flattener import flatten
 class ConfigurationBuilder:
 
     @staticmethod
-    def _make_name(path:str)->str:
+    def _make_name(path: str) -> str:
         name_arr = path.split('.')
-        if len(name_arr)>=2:
-            return name_arr[-2]+ '_' + name_arr[-1]
+        if len(name_arr) >= 2:
+            return name_arr[-2] + '_' + name_arr[-1]
         return name_arr[-1]
 
     @staticmethod
@@ -22,34 +22,35 @@ class ConfigurationBuilder:
             return "FLOAT"
         return "CHARACTER VARYING (1023)"
 
+    @staticmethod
+    def build_configuration(relation_config_path: str, mapping_config_path: str, mongo_host: str,
+                            mongo_database: str, mongo_collection: str,
+                            mongo_port: int = None, mongo_user: str = None,
+                            mongo_password: str = None):
+        mongo_client = pymongo.MongoClient(host=mongo_host, port=mongo_port, username=mongo_user,
+                                           password=mongo_password)
+        db = mongo_client[mongo_database]
+        collie = db[mongo_collection]
+        try:
+            example_document = collie.find()[0]
+        except:
+            raise FileExistsError("There was a issue getting an example document! Please check the collection {}",
+                                  mongo_collection)
+        example_document = flatten(example_document)[0]
+        configuration = {}
+        for key, value in example_document.items():
+            if key not in configuration:
+                name = ConfigurationBuilder._make_name(key)
+                typ = ConfigurationBuilder._guess_type(value)
+                configuration[key] = name + " " + typ
+        configuration = {mongo_collection: configuration}
+        with open(mapping_config_path, "w") as file:
+            configuration = json.dumps(configuration, indent=4)
+            file.write(configuration)
+        with open(relation_config_path, "w") as file:
+            json.dump({mongo_collection: {}}, file)
+        return
 
-def build_configuration(relation_config_path: str, mapping_config_path: str, mongo_host: str,
-                        mongo_database: str, mongo_collection: str,
-                        mongo_port: int = None, mongo_user: str = None,
-                        mongo_password: str = None):
-    mongo_client = pymongo.MongoClient(host=mongo_host, port=mongo_port, username=mongo_user,
-                                       password=mongo_password)
-    db = mongo_client[mongo_database]
-    collie = db[mongo_collection]
-    try:
-        example_document = collie.find()[0]
-    except:
-        raise FileExistsError("There was a issue getting an example document! Please check the collection {}",
-                              mongo_collection)
-    example_document = flatten(example_document)[0]
-    configuration = {}
-    for key,value in example_document.items():
-        if key not in configuration:
-            name = ConfigurationBuilder._make_name(key)
-            typ = ConfigurationBuilder._guess_type(value)
-            configuration[key] = name + " " + typ
-    configuration = {mongo_collection:configuration}
-    with open(mapping_config_path, "w") as file:
-        configuration = json.dumps(configuration, indent=4)
-        file.write(configuration)
-    with open(relation_config_path, "w") as file:
-        json.dump({mongo_collection:{}}, file)
-    return
 
 if __name__ == "__main__":
     ConfigurationBuilder.build_configuration("relations.json",
