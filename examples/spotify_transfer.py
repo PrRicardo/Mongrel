c6155faf2df440894e2142ad466644580303acba
Ricardo <ricardo.prida@breos.com>
2024-07-25 16:22:02 +0200

    path after merge

--- examples/spotify_transfer.py
+++ examples/spotify_transfer.py
@@ -1,13 +1,16 @@
 """
 Example usage of the transferer applied to the Spotify use case
 """
-
+import json
 import os
-from mongrel import transfer_data_from_mongo_to_postgres
+from src.mongrel_transferrer.mongrel import transfer_data_from_mongo_to_postgres
 
 if __name__ == "__main__":
-    transfer_data_from_mongo_to_postgres("spotify_relations.json",
-                                         "spotify_mappings.json", mongo_host="localhost",
-                                         mongo_database="hierarchical_relational_test", mongo_collection="test_tracks",
-                                         sql_host='127.0.0.1', sql_database='spotify', sql_user='postgres',
-                                         sql_port=35433, sql_password=os.getenv("PASSWORD"))
+    with open("spotify_relations.json") as relations:
+        with open("spotify_mappings.json") as mappings:
+            transfer_data_from_mongo_to_postgres(json.load(relations),
+                                                 json.load(mappings), mongo_host="localhost",
+                                                 mongo_database="hierarchical_relational_test",
+                                                 mongo_collection="test_tracks",
+                                                 sql_host='127.0.0.1', sql_database='spotify', sql_user='postgres',
+                                                 sql_port=35433, sql_password=os.getenv("PASSWORD"))
