72126ef3d0b351a8658ceb7bd6580e99eba959af
Ricardo <mail.prida@gmail.com>
2024-07-26 12:24:50 +0200

    incorporation of pylint feedback

--- examples/spotify_transfer.py
+++ examples/spotify_transfer.py
@@ -6,11 +6,11 @@ import os
 from src.mongrel_transferrer.mongrel import transfer_data_from_mongo_to_postgres
 
 if __name__ == "__main__":
-    with open("spotify_relations.json") as relations:
-        with open("spotify_mappings.json") as mappings:
+    with open("spotify_relations.json", encoding="utf-8") as relations:
+        with open("spotify_mappings.json", encoding="utf-8") as mappings:
             transfer_data_from_mongo_to_postgres(json.load(relations),
                                                  json.load(mappings), mongo_host="localhost",
                                                  mongo_database="hierarchical_relational_test",
                                                  mongo_collection="test_tracks",
                                                  sql_host='127.0.0.1', sql_database='spotify', sql_user='postgres',
-                                                 sql_port=35433, sql_password=os.getenv("PASSWORD"))
+                                                 sql_port=5432, sql_password=os.getenv("PASSWORD"))
